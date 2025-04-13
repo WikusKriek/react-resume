9a5d9526cc01c442639f4daf913486bfa53653df
wikuskriek <wikus.kriek.2w@gmail.com>
2025-04-13 14:31:41 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -30,7 +30,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/nextjs-github-pages",
+  basePath: "/react-resume",
   images: {
     unoptimized: true,
     remotePatterns: [
